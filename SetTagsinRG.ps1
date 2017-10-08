ceb3d2ae80137b7f0fb1121f4d9f379fe801c1f2
Robert Kuehfus <rokuehfu@microsoft.com>
2017-10-08 16:01:58 -0400

    Powershell Script used to set Azure Tags

---
+++ SetTagsinRG.ps1
@@ -0,0 +1,21 @@
+﻿#Sample code snippits for tagging VMs in Azure
+
+$rgName = '<ResourceGroupName>'
+$subscriptionId = '<SubIDGUID>'
+
+Login-AzureRmAccount
+
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
+$groups = Get-AzureRmResourceGroup -Name $rgName
+
+#Tag all resources in a resource group
+foreach ($g in $groups) 
+{
+    Find-AzureRmResource -ResourceGroupNameEquals $g.ResourceGroupName | ForEach-Object {Set-AzureRmResource -ResourceId $_.ResourceId -Tag @{ Owner="MSP"; Environment="Infra";Project="Hackathon" } -Force } 
+}
+
+
+
+#use this command to set the tags on a single VM
+#Get-AzureRmResource -ResourceGroupName $g.ResourceGroupName -ResourceName <VMname> -ResourceType Microsoft.Compute/virtualMachines | Set-AzureRmResource -Tag @{ App="RDS"; Environment="Demo"; TimeZone="EST"; StartTime="8:00"; EndTime="18:00" } -Force
