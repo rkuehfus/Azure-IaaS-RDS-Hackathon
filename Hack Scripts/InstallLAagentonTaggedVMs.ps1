73e7e4b9266ad03e0c665e125b803d0954de7fed
Robert Kuehfus <rokuehfu@microsoft.com>
2017-10-12 09:50:58 -0400

    Updated guide and added script for installing agents

---
+++ Hack Scripts/InstallLAagentonTaggedVMs.ps1
@@ -0,0 +1,58 @@
+﻿$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint   $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+} 
+
+$rgName = 'RDSHackathon'
+$subscriptionId = '8b2ef784-0c0d-424c-8a7a-aa1304a0366a'
+
+Select-AzureRMSubscription -SubscriptionId $subscriptionId
+
+$workspaceName = "RDSHackathon"
+$workspace = (Get-AzureRmOperationalInsightsWorkspace).Where({$_.Name -eq $workspaceName})
+
+if ($workspace.Name -ne $workspaceName)
+{
+    Write-Error "Unable to find OMS Workspace $workspaceName. Do you need to run Select-AzureRMSubscription?"
+}
+
+$workspaceId = $workspace.CustomerId
+$workspaceKey = (Get-AzureRmOperationalInsightsWorkspaceSharedKeys -ResourceGroupName $workspace.ResourceGroupName -Name $workspace.Name).PrimarySharedKey
+
+
+$rgName = 'RDSHackathon'
+$subscriptionId = '8b2ef784-0c0d-424c-8a7a-aa1304a0366a'
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
+$TagResults = (Find-AzureRmResource -TagName Project -TagValue Hackathon) | ? {$_.ResourceType -eq 'Microsoft.Compute/virtualMachines'}
+
+foreach ($Result in $TagResults){
+    $VM = Get-AzureRmVM -Name $Result.Name -ResourceGroupName $rgName
+    Set-AzureRmVMExtension -ResourceGroupName $rgName -VMName $VM.Name -Name 'MicrosoftMonitoringAgent' -Publisher 'Microsoft.EnterpriseCloud.Monitoring' -ExtensionType 'MicrosoftMonitoringAgent' -TypeHandlerVersion '1.0' -Location $VM.Location -SettingString "{'workspaceId': '$workspaceId'}" -ProtectedSettingString "{'workspaceKey': '$workspaceKey'}"
+  
+} 
+
+# For Windows VM uncomment the following line
+# Set-AzureRmVMExtension -ResourceGroupName $VMresourcegroup -VMName $VMresourcename -Name 'MicrosoftMonitoringAgent' -Publisher 'Microsoft.EnterpriseCloud.Monitoring' -ExtensionType 'MicrosoftMonitoringAgent' -TypeHandlerVersion '1.0' -Location $location -SettingString "{'workspaceId': '$workspaceId'}" -ProtectedSettingString "{'workspaceKey': '$workspaceKey'}"
+
+# For Linux VM uncomment the following line
+# Set-AzureRmVMExtension -ResourceGroupName $VMresourcegroup -VMName $VMresourcename -Name 'OmsAgentForLinux' -Publisher 'Microsoft.EnterpriseCloud.Monitoring' -ExtensionType 'OmsAgentForLinux' -TypeHandlerVersion '1.0' -Location $location -SettingString "{'workspaceId': '$workspaceId'}" -ProtectedSettingString "{'workspaceKey': '$workspaceKey'}"
